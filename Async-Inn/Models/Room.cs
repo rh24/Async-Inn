e6d62bf185af0a27cfa60eb78c2d1d27ac184f7c
rebeccahong <rebeccayhong@gmail.com>
2018-10-25 11:36:26 -0700

    Add props to Room model

--- Async-Inn/Models/Room.cs
+++ Async-Inn/Models/Room.cs
@@ -7,6 +7,12 @@ namespace AsyncInn.Models
 {
     public class Room
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Layout { get; set; }
+
+        public ICollection<HotelRooms> HotelRooms { get; set; }
+        public ICollection<RoomAmenities> RoomAmenities { get; set; }
     }
 
     enum Layout
