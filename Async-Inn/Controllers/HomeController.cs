1c9e3524889cf6fc8dd6ddf169a4c1e3441bf2d4
rebeccahong <rebeccayhong@gmail.com>
2018-10-25 10:03:54 -0700

    Add Home/Index view

--- Async-Inn/Controllers/HomeController.cs
+++ Async-Inn/Controllers/HomeController.cs
@@ -6,14 +6,14 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace Async-Inn.Controllers
+namespace AsyncInn.Controllers
 {
     public class HomeController : Controller
-{
-    // GET: /<controller>/
-    public IActionResult Index()
     {
-        return View();
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
-}
 }
