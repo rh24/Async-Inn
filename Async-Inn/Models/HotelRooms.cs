890d364e059f006f658e32e2b3eb267a1b524d89
rebeccahong <rebeccayhong@gmail.com>
2018-10-25 11:52:36 -0700

    Add props to HotelRooms entity join model with payload

--- Async-Inn/Models/HotelRooms.cs
+++ Async-Inn/Models/HotelRooms.cs
@@ -7,5 +7,14 @@ namespace AsyncInn.Models
 {
     public class HotelRooms
     {
+        public int HotelID { get; set; }
+        public decimal RoomID { get; set; }
+        public int RoomNumber { get; set; }
+        public decimal Rate { get; set; }
+        public bool PetFriendly { get; set; }
+
+        // Navigation properties
+        public Hotel Hotel { get; set; }
+        public Room Room { get; set; }
     }
 }
