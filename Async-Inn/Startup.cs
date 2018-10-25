f949bc013f9a0ea0d68410fdaa1d045763e8f133
rebeccahong <rebeccayhong@gmail.com>
2018-10-25 11:00:53 -0700

    Add IConfiguration to Startup constructor

--- Async-Inn/Startup.cs
+++ Async-Inn/Startup.cs
@@ -6,16 +6,30 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using AsyncInn.Data;
+using Microsoft.Extensions.Configuration;
 
 namespace Async_Inn
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<AsyncInnDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("AsyncInn"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
