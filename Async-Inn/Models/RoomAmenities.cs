93466a647cbf46b3c360f0e25483f1f85213718c
rebeccahong <rebeccayhong@gmail.com>
2018-10-25 11:34:49 -0700

    Add props to RoomAmenities pure join model

--- Async-Inn/Models/RoomAmenities.cs
+++ Async-Inn/Models/RoomAmenities.cs
@@ -7,5 +7,10 @@ namespace AsyncInn.Models
 {
     public class RoomAmenities
     {
+        public int AmenityID { get; set; }
+        public int RoomID { get; set; }
+
+        public Amenity Amenity { get; set; }
+        public Room Room { get; set; }
     }
 }
