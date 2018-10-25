2f75c836c43ea14af598e3ab68dc23ad5c2a02da
rebeccahong <rebeccayhong@gmail.com>
2018-10-25 10:25:56 -0700

    Add all models

--- Async-Inn/Data/AsyncInnDbContext.cs
+++ Async-Inn/Data/AsyncInnDbContext.cs
@@ -5,5 +5,19 @@ namespace AsyncInn.Data
 {
     public class AsyncInnDbContext : DbContext
     {
+        public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
+        {
+
+        }
+
+        // For composite keys and shadow properties
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<HotelRooms> HotelRooms { get; set; }
     }
 }
