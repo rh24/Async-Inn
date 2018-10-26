a3b6d1541dda40b3627d645f4085bb9adebc61ff
rebeccahong <rebeccayhong@gmail.com>
2018-10-26 11:59:40 -0700

    Add hotel data to dbcontext

--- Async-Inn/Data/AsyncInnDbContext.cs
+++ Async-Inn/Data/AsyncInnDbContext.cs
@@ -19,11 +19,50 @@ namespace AsyncInn.Data
             modelBuilder.Entity<RoomAmenities>().HasKey(
                 ce => new { ce.RoomID, ce.AmenityID }
                 );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    ID = 1,
+                    Name = "The Async Inn - Queens",
+                    Address = "49-27 219th St",
+                    Phone = "718-884-5535"
+                },
+                new Hotel
+                {
+                    ID = 2,
+                    Name = "The Async Inn - Manhattan",
+                    Address = "510 Madison Ave",
+                    Phone = "718-347-0990"
+                },
+                new Hotel
+                {
+                    ID = 3,
+                    Name = "The Async Inn - Brooklyn",
+                    Address = "1080 Altantic Ave",
+                    Phone = "347-888-8878"
+                },
+                new Hotel
+                {
+                    ID = 4,
+                    Name = "The Async Inn - Staten Island",
+                    Address = "4 Fairview Rd",
+                    Phone = "917-888-8878"
+                },
+                new Hotel
+                {
+                    ID = 5,
+                    Name = "The Async Inn - Bronx",
+                    Address = "210 Dreiser Loop",
+                    Phone = "718-616-3376"
+                }
+                );
         }
 
         public DbSet<Hotel> Hotels { get; set; }
         public DbSet<Room> Rooms { get; set; }
         public DbSet<HotelRooms> HotelRooms { get; set; }
         public DbSet<Amenity> Amenity { get; set; }
+        public DbSet<AsyncInn.Models.RoomAmenities> RoomAmenities { get; set; }
     }
 }
