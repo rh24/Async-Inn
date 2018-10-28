981b3602edebdaf476129b55d6e2b229354fb23c
rebeccahong <rebeccayhong@gmail.com>
2018-10-28 15:33:30 -0700

    Annotate enum and make public in Room entity model

--- Async-Inn/Models/Room.cs
+++ Async-Inn/Models/Room.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,12 @@ namespace AsyncInn.Models
     {
         // Model properties
         public int ID { get; set; }
+        [Required(ErrorMessage = "Please, provide a name for this room. Make it fun!")]
+        [Display(Name = "Room Name")]
         public string Name { get; set; }
+        // There is no way for the user to select blank. Is it possible to have a blank with an enum?
+        [Required(ErrorMessage = "Please, select the layout of this room.")]
+        [EnumDataType(typeof(Layout))]
         public int Layout { get; set; }
 
         // Navigation properties
@@ -19,8 +25,13 @@ namespace AsyncInn.Models
 
     public enum Layout
     {
+        [Display(Name = "Studio")]
         Studio,
+        [Display(Name = "One Bedroom")]
         OneBedroom,
-        TwoBedroom
+        [Display(Name = "Two Bedroom")]
+        TwoBedroom,
+        [Display(Name = "Penthouse Suite")]
+        Penthouse
     }
 }
