148c9727839c946a165dd2e189ead1877ff6314b
rebeccahong <rebeccayhong@gmail.com>
2018-10-28 18:15:28 -0700

    Add data annotations for Hotel model

--- Async-Inn/Models/Hotel.cs
+++ Async-Inn/Models/Hotel.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace AsyncInn.Models
 {
@@ -9,8 +7,13 @@ namespace AsyncInn.Models
     {
         // Model props
         public int ID { get; set; }
+        [Required(ErrorMessage = "Please, enter a name for the hotel.")]
+        [Display(Name = "Hotel Name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please, enter an address for the hotel.")]
         public string Address { get; set; }
+        [Required(ErrorMessage = "Please, enter the contact information.")]
+        [Display(Name = "Phone Number")]
         public string Phone { get; set; }
 
         // Navigation properties
